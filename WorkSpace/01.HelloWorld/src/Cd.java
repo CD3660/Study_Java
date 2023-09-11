47265c52599e7f25e3c12567186c2bc536a7bdfc
박찬영 <144075327+CD3660@users.noreply.github.com>
2023-09-11 17:46:27 +0900

    자바 1일차 수업
    main, syso 단축키를 이용한 코드 작성법

---
+++ WorkSpace/01.HelloWorld/src/Cd.java
@@ -0,0 +1,7 @@
+
+public class Cd {
+	public static void main(String[] args) {
+		System.out.println("CD");
+		System.out.println("asdf");
+	}
+}
---
+++ WorkSpace/01.HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+// 주석, 컴파일 하지 않는 문자
+// main : 메인 메소드, 프로그램의 시작점
+// syso :  프로그램의 명령어
+// ctrl + space : 코드의 자동완성
+
+// Q. 나의 이니셜Class를 만들고 콘소렝 이니셜 나오게 만들기
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("HelloWorldCD");
+	}
+}
