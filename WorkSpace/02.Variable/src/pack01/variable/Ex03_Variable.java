e0065d6c724ae5646af015bb9712de16fa18f7bd
박찬영 <144075327+CD3660@users.noreply.github.com>
2023-09-12 16:50:12 +0900

    11
    11

--- WorkSpace/02.Variable/src/pack01/variable/Ex03_Variable.java
+++ WorkSpace/02.Variable/src/pack01/variable/Ex03_Variable.java
@@ -1,5 +1,11 @@
 package pack01.variable;
 
 public class Ex03_Variable {
-
+	public static void main(String[] args) {
+		int value = 10;
+		int result = value + 20;
+		
+		System.out.println(result);
+		System.out.println(value);
+	}
 }
